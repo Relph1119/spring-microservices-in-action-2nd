32daa96b3c2bea5a1a4c890c44f96c326e048383
Relph1119 <huruifeng1202@163.com>
2022-11-16 17:02:02 +0800

    ✔ 提交第8章的代码

---
+++ ch08/gateway-server/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -0,0 +1,36 @@
+package com.optimagrowth.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class ResponseFilter {
+ 
+    final Logger logger =LoggerFactory.getLogger(ResponseFilter.class);
+    
+    @Autowired
+	FilterUtils filterUtils;
+ 
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            	  HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+            	  // 获取原始HTTP请求中传入的关联ID
+            	  String correlationId = filterUtils.getCorrelationId(requestHeaders);
+            	  logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+            	  // 将关联ID注入到响应中
+                  exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                  // 记录传出的请求URI
+                  logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+              }));
+        };
+    }
+}
